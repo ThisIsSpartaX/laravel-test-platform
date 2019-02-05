3828fb39a2afb460dd9c0518a1100ed4292defbc
Vladisalv Nazarov <megabotua@gmail.com>
2019-02-05 16:31:33 +0300

    Add wait list. Add notifications about new reservations

--- resources/views/admin/notifications.blade.php
+++ resources/views/admin/notifications.blade.php
@@ -2,7 +2,7 @@
 @if ($message = Session::get('success'))
 <div class="alert alert-success alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <strong>Success:</strong> {{ $message }}
+    {{ $message }}
 </div>
 @endif
 
