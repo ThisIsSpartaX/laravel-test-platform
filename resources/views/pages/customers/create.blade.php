c3bcc629451e401718db19ba624606430e5caaa3
Vladisalv Nazarov <megabotua@gmail.com>
2019-02-12 17:55:16 +0300

    add Customer Appreciation Club form

--- resources/views/pages/customers/create.blade.php
+++ resources/views/pages/customers/create.blade.php
@@ -7,10 +7,10 @@
 @section('content')
     <div class="container">
         <div class="row flex-column-reverse flex-lg-row">
-            <div class="col-12 col-md-6 text-center">
+            <div class="col-12 col-md-6">
                 @include('pages.customers.customer-form')
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 text-center">
                 <img src="{{ asset('images/gift-card-banner.png') }}" style="max-width: 300px;"/>
             </div>
         </div>
